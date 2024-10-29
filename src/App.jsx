e80c65bd7df3ec5d3936debba653d6ea21823833
Juan Palencia <juan.palencia@majesticagency.ca>
2024-10-29 19:12:21 -0400

    Lista de criptos creada, axios y css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import './App.css'
+
 function App() {
 
-  return (
-    <>
-   		<h1>Hola world!</h1>
-    </>
-  )
+	const API_URL = import.meta.env.VITE_API_URL
+
+	const [criptos, setCriptos] =	useState()
+
+	useEffect(() => {
+		axios.get(`${API_URL}/v2/assets`)
+			.then(response => setCriptos(response.data.data))
+	}, [])
+
+	if (!criptos) {
+		return <p>Cargando...</p>		
+	} 
+
+	return (
+		<>
+			<h1>Lista de cryptos</h1>
+
+			<table>
+				<tr>
+					<th>Nombre</th>
+					<th>Simbolo</th>
+					<th>Precio</th>
+				</tr>
+				{ criptos.map(({id, name, symbol, priceUsd}) => (
+					<tr key={id}>
+						<td> {name}</td>
+						<td>{symbol}</td>
+						<td>{priceUsd}</td>
+					</tr>
+				))}					
+			</table>
+		</>
+	)
 }
 
 export default App
